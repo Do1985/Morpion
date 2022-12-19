d2b039a6d280e90454e6903030c2a07b749b2845
Do1985 <dorian.telmon@laposte.net>
2022-12-19 13:23:20 +0000

    evolution favorable et progression continue du projet qui a connu un nouvel essor et une évolution fulgurante lors du passage du formateur quand l'eleve a su enfin oser demander de l'aide lors de sa noyade

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 //morpion
 
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
 
-const morpion =  [0, 1, 2, 3, 4, 5, 6, 7, 8]
+const morpion = ["", "", "", "", "", "", "", "", ""]
 
-const victory = [[0, 1, 2] [3, 4, 5] [6, 7, 8]
-                [0, 3, 6] [1, 4, 7] [2, 5, 8]
-                [0, 4, 8] [2, 4, 6]]
+const victory = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 
-let turn = 0
+let turn = document.getElementById("turn");
+turn = 0
+const allCase = []
+
+const onClick = (item) => {
+    console.log("test")
+    if (turn === 0) {
+        item.innerText = '╳';
+        turn++
+    }
+    else if (turn >= 1) {
+        item.innerText = '◯';
+        turn--
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     let caseOne = document.getElementById('0');
@@ -21,30 +42,52 @@ document.addEventListener('DOMContentLoaded', () => {
     let caseSeven = document.getElementById('6');
     let caseEight = document.getElementById('7');
     let caseNine = document.getElementById('8');
-    
-    
-    
-    caseOne.addEventListener("click", () => {
-        if (turn === 0) {
-            caseOne.innerText = '╳';
-            turn++
-        } else if (turn >= 1) {
-            caseOne.innerText = '◯';
-            turn--
-        }
-    })
+
+
+    allCase.push(caseOne, caseTwo,caseThree,caseFour,caseFive, caseSix,caseSeven, caseEight, caseNine)  
+
+
+allCase.forEach(function(item) {
+    item.addEventListener("click", () => onClick(item))
+})
+
+
 });
 
-bouttons.forEach(function(item){
-        item.addEventListener("click", () => onClick(item))
-    })
 
 morpion.forEach(possibility => {
+    if(allCase === ""){
+        alert('INSERT COINS')
         if (possibility[0] === possibility[1] && possibility[1] === possibility[2]) {
             alert('Vous avez gagné');
         }
-    })
+        else if (possibility[3] === possibility[4] && possibility[4] === possibility[5]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[6] === possibility[7] && possibility[7] === possibility[8]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[0] === possibility[3] && possibility[3] === possibility[6]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[1] === possibility[4] && possibility[4] === possibility[7]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[2] === possibility[5] && possibility[5] === possibility[8]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[2] === possibility[4] && possibility[4] === possibility[6]) {
+            alert('Vous avez gagné');
+        }
+        else if (possibility[0] === possibility[4] && possibility[4] === possibility[8]) {
+            alert('Vous avez gagné');
+        }
+        else(possibility[""] === possibility[""] && possibility[""] === possibility[""])
+        alert('Commencez à jouer');
+    }
 
+})
+*/
 /*en attente:
 
 function entierAleatoire(max, hasardMorpion)
